39099fc66bdaa94566601dcd6e6fb040fea79957
Tasneem <tasneem.jameel9@gmail.com>
2024-02-29 13:59:34 +0300

    Creating Models

---
+++ hospital/models/Service.js
@@ -0,0 +1,24 @@
+const { Schema } = require('mongoose')
+
+const serviceSchema = new Schema(
+  {
+    image: String,
+    name: String,
+    description: String,
+    phone: String,
+    price: Number,
+    doctors: [{ type: Schema.Types.ObjectId, ref: 'Doctor' }],
+    minAge: Number,
+    maxAge: Number,
+    genderInterest: {
+      type: String,
+      enum: ['Male', 'Female', 'Unknown'],
+      default: 'Unknown'
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+module.exports = serviceSchema
