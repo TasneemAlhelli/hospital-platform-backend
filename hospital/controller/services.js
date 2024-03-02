82055fb594786536967715482b9b7e0d7d1e8fe5
zahraa <zahraaalihasan279@gmail.com>
2024-03-02 11:12:07 +0300

    relevant services for user (controlller + route)

--- hospital/controller/services.js
+++ hospital/controller/services.js
@@ -1,4 +1,6 @@
-const { Service } = require('../models')
+const { Service, User } = require('../models')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 const getservices = async (req, res) => {
   try {
@@ -8,7 +10,23 @@ const getservices = async (req, res) => {
     console.log(error)
   }
 }
-
+const filterServices = async (req, res) => {
+  try {
+    const { token } = res.locals
+    let payload = jwt.verify(token, APP_SECRET)
+    let userId = payload.id
+    const user = await User.findById(userId)
+    const filterServices = await Service.find({
+      minAge: { $lte: user.age },
+      maxAge: { $gte: user.age },
+      specialization: { $in: [user.medicalConditions, 'other'] },
+      $or: [{ gender: 'all' }, { gender: user.gender }]
+    })
+    res.send(filterServices)
+  } catch (error) {
+    console.log(error)
+  }
+}
 const getservice = async (req, res) => {
   try {
     const service = await Service.findById(req.params.id).populate('doctors')
@@ -30,5 +48,6 @@ const addservice = async (req, res) => {
 module.exports = {
   getservices,
   getservice,
-  addservice
+  addservice,
+  filterServices
 }
--- hospital/routes/services.js
+++ hospital/routes/services.js
@@ -3,6 +3,8 @@ var router = express.Router()
 const servicesCtrl = require('../controller/services')
 
 router.get('/', servicesCtrl.getservices)
+router.get('/filter', servicesCtrl.filterServices)
+
 router.post('/', servicesCtrl.addservice)
 router.get('/:id', servicesCtrl.getservice)
 
