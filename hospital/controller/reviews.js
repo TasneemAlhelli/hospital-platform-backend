c0c2c315f3971c7bcb57193fd56d1c447670bc60
Tasneem <tasneem.jameel9@gmail.com>
2024-03-04 14:41:38 +0300

    Save doctor in review

--- hospital/controller/reviews.js
+++ hospital/controller/reviews.js
@@ -1,8 +1,11 @@
-const { Review } = require('../models')
+const { Review, Appointment } = require('../models')
 
 const create = async (req, res) => {
   try {
     req.body.user = res.locals.payload.id
+    const appointment = await Appointment.findById(req.body.appointment)
+    req.body.doctor = appointment.doctor
+
     const review = await Review.create(req.body)
     res.send(review)
   } catch (error) {
