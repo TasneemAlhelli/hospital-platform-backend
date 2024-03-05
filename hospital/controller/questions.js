bedb1f0dac0c56ca47a715af616249db1204f20b
zahraa <zahraaalihasan279@gmail.com>
2024-03-05 20:16:55 +0300

    populate service in question controller

--- hospital/controller/questions.js
+++ hospital/controller/questions.js
@@ -2,26 +2,41 @@ const { Question } = require('../models')
 
 const index = async (req, res) => {
   try {
-    const questions = await Question.find({}).populate('user')
+    const questions = await Question.find({})
+      .populate('user')
+      .populate('service')
     res.send(questions)
-  } catch (error) {}
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 const createQuestion = async (req, res) => {
   try {
     req.body.user = res.locals.payload.id
     const newQuestion = await Question.create(req.body)
-    const questions = await Question.find({}).populate('user')
+    const questions = await Question.find({})
+      .populate('user')
+      .populate('service')
     res.send(questions)
-  } catch (error) {}
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 const answerToQuestion = async (req, res) => {
-  console.log(req.body)
-  let question = await Question.findById(req.params.questionId)
-  const updatedQuestion = await Question.updateOne({
-    answer: req.body.answer
-  })
+  try {
+    let question = await Question.findById(req.params.questionId)
+    const updatedQuestion = await Question.updateOne({
+      answer: req.body.answer
+    })
+    const questions = await Question.find({})
+      .populate('user')
+      .populate('service')
+    res.send(questions)
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 module.exports = {
